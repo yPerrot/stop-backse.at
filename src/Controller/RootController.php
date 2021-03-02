9be43d04188e2f6915c0772fbae4aabbf8c33cee
emilweth <coucou@emi.cool>
2021-03-02 20:30:36 +0100

    feature/language-switch (#4)
    Reviewed-on: https://git.emi.cool/emilweth/stop-backse.at/pulls/4
    Co-authored-by: emilweth <coucou@emi.cool>
    Co-committed-by: emilweth <coucou@emi.cool>

---
+++ src/Controller/RootController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class RootController extends AbstractController
+{
+    public function root(Request $request): RedirectResponse
+    {
+        return $this->redirectToRoute('home', [
+            '_locale' => $request->getLocale() == 'fr' ? 'fr' : 'en'
+        ]);
+    }
+}
