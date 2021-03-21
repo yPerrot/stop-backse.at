bf5e1f6beb403ff384e5c13a03022c231d2c8aa6
Emilweth <7402764+emilweth@users.noreply.github.com>
2021-03-21 16:42:40 +0100

    Feature/language switch (#1)
    * fix automatic language switch

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -13,17 +13,10 @@ class LocaleSubscriber implements EventSubscriberInterface
     public function onKernelRequest(RequestEvent $event): void
     {
         $request = $event->getRequest();
-        if ($request->headers->has('Accept-Language')) {
-            switch ($request->headers->get('Accept-Language')) {
-                case 'fr':
-                    $request->setLocale('fr');
-
-                    break;
-
-                case 'en':
-                default:
-                    $request->setLocale('en');
-            }
+        if (\in_array('fr', $request->getLanguages(), true)) {
+            $request->setLocale('fr');
+        } else {
+            $request->setLocale('en');
         }
     }
 
